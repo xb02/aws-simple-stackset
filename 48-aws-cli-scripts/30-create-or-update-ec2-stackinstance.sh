f0bcbf6285b042379827fbb1d68d98253d856bfb
Balaji Venkataraman <xbala.ji+git@gmail.com>
2019-03-11 00:54:06 -0700

    shell script to invoke aws cli commands to create or update stack instances as necessary

---
+++ 48-aws-cli-scripts/30-create-or-update-ec2-stackinstance.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+# 
+# Filename    : aws-simple-stackset/48-aws-cli-scripts/30-create-or-update-ec2-stackinstance.sh
+# Date        : 10 Mar 2019
+# Author      : Balaji Venkataraman (xbalaji@gmail.com)
+# Description : AWS CLI command to create stack instance from simple EC2 instance stackset
+#
+
+# copy the parameters from an existing stack out, use the following command to convert to json in vim
+# g/\(\S\+\)\(\s\+\)\(\S\+\)\(.*\)/s//  {\r    "ParameterKey": "\1",\r    "ParameterValue": "\3"\r  },/gc
+
+# aws cli reference example
+# https://docs.aws.amazon.com/cli/latest/reference/cloudformation/create-stack-instances.html
+# https://docs.aws.amazon.com/cli/latest/reference/cloudformation/update-stack-instances.html
+
+
+StackSetName="create-simple-single-ec2-stackset"
+for ix in $(cat ../45-aws-cli-params/simple-ec2-stack-accounts.txt)
+do
+
+  IFS=',' read -r -a acc_reg_array <<< "${ix}"
+  AccNum=${acc_reg_array[0]}
+  RegName=${acc_reg_array[1]}
+  echo "Account number: ${AccNum}"
+  echo "Account region: ${RegName}"
+
+  CmdLine="aws --profile innovation cloudformation describe-stack-instance --stack-set-name ${StackSetName}"
+  instance_status=$(${CmdLine} --stack-instance-account ${AccNum} --stack-instance-region ${RegName} 2>/dev/null | jq '.StackInstance.Status')
+  echo "instance status: ${instance_status}"
+
+  if [ -z $instance_status ]
+  then
+    AwsCliCmd=create
+  else
+    AwsCliCmd=update
+  fi
+    
+  OperId=$(aws --profile innovation cloudformation "${AwsCliCmd}-stack-instances"       \
+      --stack-set-name ${StackSetName} --accounts "${AccNum}" --regions  "${RegName}"   \
+      --parameter-overrides file://../45-aws-cli-params/simple-ec2-params.json 2> /dev/null | jq '.OperationId')
+
+  echo "Operation id: ${OperId}"
+  while true
+  do
+    sleep 5
+    if [ -z $OperId ]; then break; fi
+
+    OperStatus=$(aws cloudformation describe-stack-set-operation --stack-set-name ${StackSetName} --operation-id ${OperId} 2>/dev/null | jq '.StackSetOperation.Status')
+    if [ -z $OperStatus ]; then break; fi
+
+    if [ ${OperStatus} != '"RUNNING"' ]; then break; fi
+  done
+
+done
